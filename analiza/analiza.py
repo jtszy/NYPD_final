eaae90d7c64d9e2a23611e1eab238ea69aa2efda
jtszy <jt.szymanski@student.uw.edu.pl>
2023-01-27 13:13:13 +0100

    setting up a repository

---
+++ analiza/analiza.py
@@ -0,0 +1,13 @@
+# czyszczenie danych
+
+# wybieranie odpowiednich lat
+
+# scalanie po krajach i latach
+
+# przeprowadzenie analiz
+
+# 1. najwieksza emisja CO2
+
+# 2. najwiekszy przychod mieszkanca
+
+# 3. najwiksze zmiany w emisji CO2
